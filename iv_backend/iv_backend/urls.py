8c96f1228a5b1ee4e3961634376480d6756cc223
ahmadileham <96009848+ahmadileham@users.noreply.github.com>
2024-10-20 17:54:20 +0800

    Modified password reset confirm for dj rest auth

--- iv_backend/iv_backend/urls.py
+++ iv_backend/iv_backend/urls.py
@@ -17,13 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from dj_rest_auth.registration.views import VerifyEmailView
+from dj_rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('dj-rest-auth/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
+    path('dj-rest-auth/password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+
     path('vault/', include('vault.urls')),
     path('password-management/', include('password_management.urls')),
     path('collaboration/', include('collaboration.urls')),
